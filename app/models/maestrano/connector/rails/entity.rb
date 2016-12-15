d5456edc5b6aa16fe3df7b97f651776a3d7d6207
Mehreen Tahir <mehreentahir18@gmail.com>
2016-12-15 15:40:43 +0500

    Pipedrive: Linked pipedrive client with connector entity & external

--- app/models/maestrano/connector/rails/entity.rb
+++ app/models/maestrano/connector/rails/entity.rb
@@ -11,41 +11,46 @@ class Maestrano::Connector::Rails::Entity < Maestrano::Connector::Rails::EntityB
   # Return an array of entities from the external app
   def get_external_entities(external_entity_name, last_synchronization_date = nil)
     Maestrano::Connector::Rails::ConnectorLogger.log('info', @organization, "Fetching #{Maestrano::Connector::Rails::External.external_name} #{self.class.external_entity_name.pluralize}")
-    # TODO
-    # This method should return only entities that have been updated since the last_synchronization_date
-    # It should also implements an option to do a full synchronization when @opts[:full_sync] == true or when there is no last_synchronization_date
-    # It should also support [:__limit] and [:__skip] opts, meaning that if they are present, it should return only @[:__limit] entities while skipping the @opts[:__skip] firsts
-    # Maestrano::Connector::Rails::ConnectorLogger.log('info', @organization, "Received data: Source=#{Maestrano::Connector::Rails::External.external_name}, Entity=#{self.class.external_entity_name}, Response=#{entities}")
+
+    last_synchronization_date = nil if @opts[:full_sync]
+    fetch_opts = {
+      last_sync_date: last_synchronization_date,
+    }
+    entities = @external_client.perform(:fetch, external_entity_name, fetch_opts)
+
+    Maestrano::Connector::Rails::ConnectorLogger.log('info', @organization, "Received data: Source=#{Maestrano::Connector::Rails::External.external_name}, Entity=#{self.class.external_entity_name}, Response=#{entities}")
+    entities
   end
 
   def create_external_entity(mapped_connec_entity, external_entity_name)
     Maestrano::Connector::Rails::ConnectorLogger.log('info', @organization, "Sending create #{external_entity_name}: #{mapped_connec_entity} to #{Maestrano::Connector::Rails::External.external_name}")
-    # TODO
-    # This method creates the entity in the external app and returns the created external entity
+
+    create_opts = {
+      params: mapped_connec_entity,
+    }
+    @external_client.perform(:create, external_entity_name, create_opts)
   end
 
   def update_external_entity(mapped_connec_entity, external_id, external_entity_name)
     Maestrano::Connector::Rails::ConnectorLogger.log('info', @organization, "Sending update #{external_entity_name} (id=#{external_id}): #{mapped_connec_entity} to #{Maestrano::Connector::Rails::External.external_name}")
-    # TODO
-    # This method updates the entity with the given id in the external app and returns the created external entity
+
+    update_opts = {
+      entity_id: external_id,
+      params: mapped_connec_entity,
+    }
+    @external_client.perform(:update, external_entity_name, update_opts)
   end
 
   def self.id_from_external_entity_hash(entity)
-    # TODO
-    # This method return the id from an external_entity_hash
-    # e.g entity['id']
+    entity['id']
   end
 
   def self.last_update_date_from_external_entity_hash(entity)
-    # TODO
-    # This method return the last update date from an external_entity_hash
-    # e.g entity['last_update']
+    entity['update_time'] || entity['modified']
   end
 
   def self.creation_date_from_external_entity_hash(entity)
-    # TODO
-    # This method return the creation date from an external_entity_hash
-    # e.g entity['created_at']
+    entity['add_time'] || entity['created']
   end
 
   def self.inactive_from_external_entity_hash?(entity)
