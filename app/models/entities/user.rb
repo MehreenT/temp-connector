afa1883be439cbe25023f5e4eefe5a89f4e9894b
Mehreen Tahir <mehreen.tahir@square63.com>
2017-01-09 17:20:58 +0500

    User: Added mapping & test case for user entity

---
+++ app/models/entities/user.rb
@@ -0,0 +1,49 @@
+class Entities::User < Maestrano::Connector::Rails::Entity
+  def self.connec_entity_name
+    'App_User'
+  end
+
+  def self.external_entity_name
+    'User'
+  end
+
+  def self.mapper_class
+    UserMapper
+  end
+
+  def self.object_name_from_connec_entity_hash(entity)
+    "#{entity['first_name']} #{entity['last_name']}"
+  end
+
+  def self.object_name_from_external_entity_hash(entity)
+    entity['name']
+  end
+
+  def self.can_update_external?
+    false
+  end
+end
+
+class UserMapper
+  extend HashMapper
+
+  # Mapping to PipeDrive
+  after_normalize do |input, output|
+    output[:name] = [input['first_name'], input['last_name']].join(' ').to_s.strip
+
+    output
+  end
+
+  # Mapping to Connec!
+  after_denormalize do |input, output|
+    if input['name']
+      output[:first_name] = input['name'].split(' ', 2).first
+      output[:last_name] = input['name'].split(' ', 2).last
+    end
+    output
+  end
+
+  map from('role'), to('role_id')
+  map from('email/address'), to('email')
+  map from('is_admin'), to('is_admin')
+end
---
+++ spec/models/entities/user_spec.rb
@@ -0,0 +1,78 @@
+require 'spec_helper'
+
+describe Entities::User do
+
+  describe 'class methods' do
+    subject { Entities::User }
+
+    it { expect(subject.connec_entity_name).to eql('App_Users') }
+    it { expect(subject.external_entity_name).to eql('User') }
+    it { expect(subject.object_name_from_connec_entity_hash({ first_name: 'abc', last_name: 'def' }.with_indifferent_access)).to eql('abc def') }
+    it { expect(subject.object_name_from_external_entity_hash({ name: 'xyz' }.with_indifferent_access)).to eql('xyz') }
+    it { expect(subject.can_update_external?).to eql(false) }
+  end
+
+  describe 'instance methods' do
+    let(:organization) { create(:organization) }
+    let(:connec_client) { Maestrano::Connector::Rails::ConnecHelper.get_client(organization) }
+    let(:external_client) { Maestrano::Connector::Rails::External.get_client(organization) }
+    let(:opts) { {} }
+    subject { Entities::User.new(organization, connec_client, external_client, opts) }
+
+
+    describe 'external to connec!' do
+      let(:external_hash) {
+        {
+          id: 'id-person1',
+          name: 'user 1',
+          role_id: 'role 1',
+          email: 'mail@gmail.com',
+          is_admin: true,
+          add_time: 2285824789,
+          update_time: 2285824799,
+        }.with_indifferent_access
+      }
+
+      let (:connec_hash) {
+        {
+          id: [{ id: 'id-person1', provider: organization.oauth_provider, realm: organization.oauth_uid }],
+          first_name: 'user',
+          last_name: '1',
+          role: 'role 1',
+          email: {
+            address: 'mail@gmail.com',
+          },
+          is_admin: true,
+        }.with_indifferent_access
+      }
+
+      it { expect(subject.map_to_connec(external_hash)).to eql(connec_hash) }
+    end
+
+    describe 'connec to external' do
+      let(:connec_hash) {
+        {
+          first_name: 'user',
+          last_name: '2',
+          role: 'role 2',
+          email: {
+            address: 'mail@gmail.com',
+          },
+          is_admin: false,
+        }.with_indifferent_access
+      }
+
+      let(:external_hash) {
+        {
+          name: 'user 2',
+          role_id: 'role 2',
+          email: 'mail@gmail.com',
+          is_admin: false,
+        }.with_indifferent_access
+      }
+
+      it { expect(subject.map_to_external(connec_hash)).to eql(external_hash) }
+    end
+  end
+
+end
